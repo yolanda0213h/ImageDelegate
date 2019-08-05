069fbbd0103947af3b6bfb8292f48961d990556c
yolanda0213 <yolanda0213h@gmail.com>
2019-08-05 18:05:50 +0800

    photoLibrary done

--- ImageDelegate/ViewController.swift
+++ ImageDelegate/ViewController.swift
@@ -8,13 +8,48 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,UIImagePickerControllerDelegate,UINavigationControllerDelegate {
 
+    @IBOutlet weak var buttonImage: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    func photopicker(){
+        let photoController = UIImagePickerController()
+        photoController.delegate = self
+        photoController.sourceType = .photoLibrary
+        present(photoController, animated: true, completion: nil)
+    }
+    func camera(){
+        let cameraController = UIImagePickerController()
+        cameraController.delegate = self
+        cameraController.sourceType = .camera
+        present(cameraController, animated: true, completion: nil)
+    }
+    @IBAction func ImageButton(_ sender: UIButton) {
+        let controller = UIAlertController(title: nil, message: nil, preferredStyle: .actionSheet)
+        let cameraAction = UIAlertAction(title: "開啟相機拍照", style: .default) { (_) in
+            self.camera()
+        }
+        let libraryAction = UIAlertAction(title: "從相簿中選擇", style: .default) { (_) in
+            self.photopicker()
+        }
+        let deleteAction = UIAlertAction(title: "刪除", style: .destructive) { (_) in
+            sender.setImage(UIImage(named: "user"), for: .normal)
+        }
+        let cancelAction = UIAlertAction(title: "取消", style: .cancel, handler: nil)
+        controller.addAction(cameraAction)
+        controller.addAction(libraryAction)
+        controller.addAction(deleteAction)
+        controller.addAction(cancelAction)
+        present(controller, animated: true, completion: nil)
+    }
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        let image = info[.originalImage] as? UIImage
+        buttonImage.setImage(image, for: .normal)
+        dismiss(animated: true, completion: nil)
+    }
 
 }
 
